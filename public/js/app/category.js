0d7d942cd816a4b6b78fb892470b69b052cff3e2
zzmshinnosuke <1608295563@qq.com>
2022-02-23 21:23:29 +0800

    new category.js,categories of three times data are merged

--- public/js/app/category.js
+++ public/js/app/category.js
@@ -1,68 +1,81 @@
 'use strict';
 Paras.foreground = [
-'zebra',
-'giraffe', 
-'cow', 
-'sheep', 
-'elephant', 
-'bird', 
-'horse', 
-'bear', 
-'dog', 
-'car', 
-'bench', 
-'truck', 
-'cat', 
-'boat', 
-'apple', 
-'bowl',
-'umbrella', 
-'chair', 
-'bicycle', 
-'potted plant', 
-'banana', 
-'motorcycle', 
-'backpack', 
-'bus', 
-'bottle', 
-'carrot', 
-'train', 
-'fire hydrant', 
-'traffic light', 
-'teddy bear', 
-'dining table', 
-'skateboard', 
-'airplane', 
-'clock', 
-'tennis racket', 
-'pizza', 
-'laptop', 
-'mouse', 
-'keyboard', 
-'book', 
-'tv', 
-'cup', 
-'couch',
-'fore_others',
+'person', //8572
+'zebra', //2847
+'giraffe', //2335
+'cow', //1959
+'sheep', //1856
+'elephant', //1812
+'airplane', //1277
+'car', //1103
+'tennis racket', //1042
+'horse', //990
+'bus', //902
+'bird', //874
+'skis', //869
+'skateboard', //814
+'sports ball', //746
+'baseball bat', //615
+'baseball glove', //607
+'snowboard', //494
+'truck', //488
+'motorcycle', //481
+'frisbee', //412
+'dog', //319
+'bear', //319
+'backpack', //199
+'surfboard', //194
+'kite', //180
+'bicycle', //176
+'chair', //93
+'stop sign', //81
+'traffic light', //81
+'bench', //72
+'train', //59
+'handbag', //49
+'cat', //45
+'boat', //38
+'umbrella', //36
+'potted plant', //33
+'sun', //27
+'tie', //25
+'fire hydrant', //20
+'bottle', //15
+'apple', //12
+'fore_others', //842
+//'suitcase', 10
+//'cell phone', 8
+//'bowl', 8
+//'parking meter', 5
+//'couch', 4
+//'teddy bear', 4
+//'dining table', 4
+//'cup', 3
+//'clock', 3
+//'banana', 2
+//'book', 2
+//'mouse', 2
+//'orange', 2
+//'laptop', 1
+//'carrot', 1
+//'vase', 1
 ];
+
 
 Paras.background = [
 //树木、草地、河流、太阳、云、大石块、山地、道路、明显分界位置的线条、围栏、房子、其他
-"playground",
-"snowfield",
-"tree", 
-"grass", 
-"grasses",
-"river", 
-"sun", 
-"cloud", 
-"stone", 
-"stones",
-"mountain", 
-"road", 
-"boundary", 
-"fence",
-"house",
-"others",
+'tree', //21983+511
+'cloud', //7784
+'grass', //7706
+'boundary', //4886
+'road', //2571
+'house', //2179
+'fence', //2037
+'stone', //1210+647
+'playground', //1136
+'mountain', //1078
+'snowfield', //1062
+'river', //826
+'others', //4489
 "useless",
 ];
